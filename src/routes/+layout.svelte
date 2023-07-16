9c0fa06551474e8d89318a883bb88f6ec6fd6055
Anuolu-2020 <Anuolu1645@gmail.com>
2023-07-16 17:25:07 +0100

    deploy 1 commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
   import Header from "./header.svelte";
   import Footer from "./footer.svelte";
-  import PageTransition from './transition.svelte'
+  import PageTransition from "./transition.svelte";
   import "open-props/style";
   import "open-props/normalize";
   import "open-props/buttons";
   import "../app.css";
-  export let data
+  export let data;
 </script>
 
 <div class="layout">
   <Header />
   <main>
     <PageTransition url={data.url}>
-			<slot />
-		</PageTransition>
-    </main>
+      <slot />
+    </PageTransition>
+  </main>
   <Footer />
 </div>
 
